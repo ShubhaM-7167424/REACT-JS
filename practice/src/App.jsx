f80a0e367b6ef5a1f402ead9728153fc3b4cf896
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-10-10 20:24:12 +0530

    load new countries

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -11,17 +11,27 @@ function App() {
         { name: "USA", capital: "Washington DC", population: "600M" },
     ]);
 
+    function loadNewCountries() {
+        setCountries([
+            { name: "Germany", capital: "Berlin", population: "90M" },
+            { name: "China", capital: "Beijing", population: "1.4B" },
+            { name: "Japan", capital: "Tokyo", population: "126M" },
+        ]);
+    }
+
     return (
         <>
+        <button onClick={loadNewCountries}>Load New Countries</button>
+
             {
                 countries.map((country)=>{
                     return (
                         <Country name={country.name} capital={country.capital} population={country.population} />
                     )
                 })
-
-             
+   
             }
+
         </>
     );
 }
