79bd4bfbd68b1145a91034dde2a28a6cd9f92d77
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-10-10 12:48:44 +0530

    destructuring using props

--- practice/src/Product.jsx
+++ practice/src/Product.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 const Product = (props) => {
+
+  let {name, price, specs, category} = props
+
   return (
     <div className='product'>
-      <h2>{props.name}</h2>
-      <h2>{props.price}</h2>
-      <h2>{props.specs.ram}</h2>
-      <h2>{props.category[0]}</h2>
+      <h2>{name}</h2>
+      <h2>{price}</h2>
+      <h2>{specs.ram}</h2>
+      <h2>{category[0]}</h2>
     </div>
   )
 }
