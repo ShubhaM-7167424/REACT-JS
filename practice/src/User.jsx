bf7bb5a3252505604ba77c96e25539f0126f14ed
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-10-10 17:20:13 +0530

    event handler

--- practice/src/User.jsx
+++ practice/src/User.jsx
@@ -3,23 +3,23 @@ import { useState } from 'react'
 
 const User = () => {
 
-        let [name, setName] = useState('rahul')
-        let [age, setAge] = useState(20)
-
-        function changeName() {
-                setName('shubham')
-                console.log(name);          
-        }
-        function changeAge() {
-                setAge(89)
-        }
+      function doSomething(planet) {
+        console.log('hello ' +planet);
+        
+      }
 
   return (
     <div className='user'>
-        <h2>{name}</h2>
-        <h2>{age}</h2>
-        <button onClick={changeName}>Change Name</button>
-        <button onClick={changeAge}>Change Age</button>
+        
+        {/* this doSomething function is already getting called, which is not good */}
+      <button onClick={doSomething('Pluto')}>Do Something</button>
+
+        {/* 1st way to solve this */}
+      <button onClick={doSomething.bind(this, 'Pluto')}>Do Something</button>
+
+        {/* 2nd Way to solve this */}
+      <button onClick={() => {doSomething('Pluto')}}>Do Something</button>
+
     </div>
   )
 }
