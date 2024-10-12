35fca5ca24386c33bf31c87376a5fa705922669c
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-10-12 17:44:26 +0530

    use Effect

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,43 +1,19 @@
 import { useState } from "react";
 import "./App.css";
-import Country from "./Country";
 import Product from "./Product";
-import User from "./User";
 
 function App() {
-
-    let [isLoggedIn, setIsLoggedIn] = useState(true)
-
-    let [countries, setCountries] = useState([
-        { name: "Nepal", capital: "kathmandu", population: "230M" },
-        { name: "India", capital: "Delhi", population: "1B" },
-        { name: "USA", capital: "Washington DC", population: "600M" },
-    ]);
-
-    function loadNewCountries() {
-        setCountries([
-            { name: "Germany", capital: "Berlin", population: "90M" },
-            { name: "China", capital: "Beijing", population: "1.4B" },
-            { name: "Japan", capital: "Tokyo", population: "126M" },
-        ]);
-    }
+    let [showProduct, setShowProduct] = useState(true);
 
     return (
         <>
-        <button onClick={loadNewCountries}>Load New Countries</button>
+            <h1>UseEffect Demo</h1>
 
-            {
-                isLoggedIn ? countries.map((country)=>{
-                    return (
-                        <Country name={country.name} capital={country.capital} population={country.population} />
-                    )
-                }) : <h1>Logged Out</h1>
-            }
+            <button onClick={()=> {setShowProduct(true)}}>Show</button>
+            <button onClick={()=> {setShowProduct(false)}}>Hide</button>
 
 
-
-          
-
+            {showProduct === true ? <Product /> : <h1>Product Not Found</h1>}
         </>
     );
 }
--- practice/src/Product.jsx
+++ practice/src/Product.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const Product = (props) => {
+const Product = () => {
+    const [name, setName] = useState("Samsung M23");
+    const [price, setPrice] = useState(40000);
 
-  let {name, price, specs, category} = props
+    useEffect(() => {
+        console.log("useEffect called");
+        
+        return () => {
+            console.log("Component UnMounted");
+        };
+    }, [price]);
 
-  return (
-    <div className='product'>
-      <h2>{name}</h2>
-      <h2>{price}</h2>
-      <h2>{specs.ram}</h2>
-      <h2>{category[0]}</h2>
-    </div>
-  )
-}
+    return (
+        <div className="product">
+            <h1>{name}</h1>
+            <h2>{price}</h2>
+            <button
+                onClick={() => {
+                    setName("Iphone 15");
+                }}
+            >
+                change Name
+            </button>
+            <button onClick={() => setPrice(70000)}>change Price</button>
+        </div>
+    );
+};
 
-export default Product
+export default Product;
