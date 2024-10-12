cf195bfa0fb3a00c797ebed4dc8db3e478803b99
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-10-12 17:44:58 +0530

    use effect

--- practice/src/Product.jsx
+++ practice/src/Product.jsx
@@ -7,6 +7,12 @@ const Product = () => {
     useEffect(() => {
         console.log("useEffect called");
         
+        // 1. By default useffect is called for every render and re-render
+        // 2. If you pass a second param as a blank array it will be only called for first render
+        // 3. If you pass variables in dependecy array - use effect will still be called for the first render
+        //    but now it  will also be called if the value of  that variable changes changes in the dependecy array
+        // 4. You can have multiple useffect in a single component
+
         return () => {
             console.log("Component UnMounted");
         };
