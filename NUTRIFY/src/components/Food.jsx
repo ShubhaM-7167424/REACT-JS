62a5728a936f479dd7e834a7e2f79ebb483b927f
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-11-25 20:39:46 +0530

    calculate nutrients based on food eaten

--- NUTRIFY/src/components/Food.jsx
+++ NUTRIFY/src/components/Food.jsx
@@ -1,43 +1,75 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 const Food = (props) => {
-      const {name, calories, protein, fat,  carbohydrates,fiber, imageUrl} = props.food
-      const [grams, setGrams] = useState(100)
-      
-  return (
-      <div className="food">
-          <div className="food-img">
-            <img src={imageUrl} className='food-image' />
-          </div>
+    const [eatenQuantity, setEatenQuantity] = useState(100);
+    const [food, setFood] = useState({});
+    const [foodInitial, setFoodInitial] = useState(props.food);
 
-          <h3>{name} ({calories}Cal for {grams}Gm) </h3>
+    useEffect(() => {
+        setFood(props.food);
+    }, [props.food]);
 
-          <div className="nutrient">
-              <p className="n-title">Protein</p>
-              <p className="n-value">{protein}g</p>
-          </div>
+    function calculateMacros(event) {
+        if (event.target.value.length !== 0) {
+            let quantity = Number(event.target.value);
+            setEatenQuantity(quantity);
 
-          <div className="nutrient">
-              <p className="n-title">Carbs</p>
-              <p className="n-value">{carbohydrates}g</p>
-          </div>
+            let copyFood = { ...food };
 
-          <div className="nutrient">
-              <p className="n-title">Fat</p>
-              <p className="n-value">{fat}g</p>
-          </div>
+            copyFood.protein = (foodInitial.protein * quantity) / 100;
+            copyFood.carbohydrates = (foodInitial.carbohydrates * quantity) / 100;
+            copyFood.fat = (foodInitial.fat * quantity) / 100;
+            copyFood.fiber = (foodInitial.fiber * quantity) / 100;
+            copyFood.calories = (foodInitial.calories * quantity) / 100;
 
-          <div className="nutrient">
-              <p className="n-title">Fiber</p>
-              <p className="n-value">{fiber}g</p>
-          </div>
+            setFood(copyFood);
+        } else {
+            setFood(foodInitial);
+            setEatenQuantity(100);
+        }
+    }
 
-          <input type="number" className="inp" placeholder="Quantity in Grams" />
+    return (
+        <div className="food">
+            <div className="food-img">
+                <img src={food.imageUrl} className="food-image" />
+            </div>
 
-          <button className="btn">track this food</button>
-      </div>
-  );
-}
+            <h3>
+                {food.name} ({food.calories}Cal for {eatenQuantity}Gm){" "}
+            </h3>
 
-export default Food
+            <div className="nutrient">
+                <p className="n-title">Protein</p>
+                <p className="n-value">{food.protein}g</p>
+            </div>
+
+            <div className="nutrient">
+                <p className="n-title">Carbs</p>
+                <p className="n-value">{food.carbohydrates}g</p>
+            </div>
+
+            <div className="nutrient">
+                <p className="n-title">Fat</p>
+                <p className="n-value">{food.fat}g</p>
+            </div>
+
+            <div className="nutrient">
+                <p className="n-title">Fiber</p>
+                <p className="n-value">{food.fiber}g</p>
+            </div>
+
+            <input
+                type="number"
+                className="inp"
+                onChange={calculateMacros}
+                placeholder="Quantity in Grams"
+            />
+
+            <button className="btn">track this food</button>
+        </div>
+    );
+};
+
+export default Food;
