3e735ba9e8eb2a87fecccaf08d54b22ef37e58aa
Shubham Singh <shubhamsingh7167424@gmail.com>
2025-01-03 16:25:52 +0530

    import and export

--- newapp/src/App.jsx
+++ newapp/src/App.jsx
@@ -1,24 +1,17 @@
+import React from "react";
+import { Header2, Header3 } from "./Components/Header";
+import Header from "./Components/Header";
+
 function App() {
     return (
         <div>
-            Hello Sir
-            <Mobile nameForClass="mobile" name="lava 6" salary={45000} />
-            <Mobile nameForClass="phone" name="iphone 13" />
-            <Mobile nameForClass="tele" name="vivo v25" salary={5000000} />
+            hihihihihihihihihihih
+            <Header />
+            <Header2 />
+            <Header3 />
+            hahahahaahaahaahaahaaha
         </div>
     );
-}
-
-function Mobile({ nameForClass, name, salary = 1000 }) {
-    return (
-        <div className={nameForClass}>
-            {name}: ${salary}
-        </div>
-    );
-}
-
-function MyComponent() {
-    return <h1>Namaste</h1>;
 }
 
 export default App;
---
+++ newapp/src/Components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <>
+            <h1>Hi Guys</h1>
+        </>
+    );
+}
+function Header2() {
+    return <h1>Header 2</h1>;
+}
+function Header3() {
+    return <h1>Header 3</h1>;
+}
+
+export default Header;
+export { Header2, Header3 };
